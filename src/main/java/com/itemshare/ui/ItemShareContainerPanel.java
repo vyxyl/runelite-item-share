bc73229ed2736fcc23eb764512d9a4681b9cf6b0
vyxyl <>
2021-11-17 11:45:52 -0500

    Improve styling of empty state messages

--- src/main/java/com/itemshare/ui/ItemShareContainerPanel.java
+++ src/main/java/com/itemshare/ui/ItemShareContainerPanel.java
@@ -1,34 +1,62 @@
 package com.itemshare.ui;
 
 import com.itemshare.model.ItemShareContainer;
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
-import javax.swing.JTextArea;
+import javax.swing.JTextPane;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
+import javax.swing.text.StyledDocument;
 import net.runelite.client.game.ItemManager;
 
 public class ItemShareContainerPanel extends JPanel
 {
 	private final ItemShareList list = new ItemShareList();
-	private final JTextArea noItemsMessage;
+	private final JTextPane noItemsMessage;
+	private final JTextPane actionMessage;
 
 	protected ItemShareContainerPanel(String containerName)
 	{
 		super(false);
 		setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
 
-		noItemsMessage = new JTextArea(2, 20);
-		noItemsMessage.setText("No " + containerName + " items are saved. The player needs to make a change to their " + containerName + " to update this list");
-		noItemsMessage.setWrapStyleWord(true);
-		noItemsMessage.setLineWrap(true);
-		noItemsMessage.setOpaque(false);
-		noItemsMessage.setEditable(false);
-		noItemsMessage.setFocusable(false);
+		noItemsMessage = new JTextPane();
+		noItemsMessage.setText("No " + containerName + " items are saved");
+		noItemsMessage.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
+
+		actionMessage = new JTextPane();
+		actionMessage.setText("The player needs to make a change to their " + containerName + " to update this list");
+		actionMessage.setBorder(BorderFactory.createEmptyBorder(10, 10, 0, 10));
+
+		setMessageSettings(noItemsMessage);
+		setMessageSettings(actionMessage);
+
+		setCenterStyle(noItemsMessage);
+		setCenterStyle(actionMessage);
 
 		list.setVisible(false);
 		noItemsMessage.setVisible(true);
+		actionMessage.setVisible(true);
 
 		add(list);
 		add(noItemsMessage);
+		add(actionMessage);
+	}
+
+	private void setMessageSettings(JTextPane actionMessage)
+	{
+		actionMessage.setOpaque(false);
+		actionMessage.setEditable(false);
+		actionMessage.setFocusable(false);
+	}
+
+	private void setCenterStyle(JTextPane textPane)
+	{
+		StyledDocument style = textPane.getStyledDocument();
+		SimpleAttributeSet center = new SimpleAttributeSet();
+		StyleConstants.setAlignment(center, StyleConstants.ALIGN_CENTER);
+		style.setParagraphAttributes(0, style.getLength(), center, false);
 	}
 
 	public void update(ItemManager itemManager, ItemShareContainer container)
@@ -37,6 +65,7 @@ public class ItemShareContainerPanel extends JPanel
 		{
 			list.setVisible(false);
 			noItemsMessage.setVisible(true);
+			actionMessage.setVisible(true);
 		}
 		else
 		{
@@ -44,6 +73,7 @@ public class ItemShareContainerPanel extends JPanel
 
 			list.setVisible(true);
 			noItemsMessage.setVisible(false);
+			actionMessage.setVisible(false);
 		}
 
 		repaint();
