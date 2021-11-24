894f9e26ecf1ff69ac65defdd7fe65146fa71e7e
vyxyl <>
2021-11-24 12:00:24 -0500

    Add tab icons

--- src/main/java/com/itemshare/ui/ItemSharePlayerPanel.java
+++ src/main/java/com/itemshare/ui/ItemSharePlayerPanel.java
@@ -1,15 +1,27 @@
 package com.itemshare.ui;
 
+import com.itemshare.ItemSharePlugin;
+import static com.itemshare.constant.ItemShareConstants.ICON_BANK;
+import static com.itemshare.constant.ItemShareConstants.ICON_CLOSE_BUTTON;
+import static com.itemshare.constant.ItemShareConstants.ICON_EQUIPMENT;
+import static com.itemshare.constant.ItemShareConstants.ICON_INVENTORY;
+import static com.itemshare.constant.ItemShareConstants.ICON_SETTINGS_BUTTON;
 import com.itemshare.model.ItemSharePlayer;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Image;
+import static java.awt.Image.SCALE_SMOOTH;
 import javax.swing.BoxLayout;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import net.runelite.client.game.ItemManager;
+import net.runelite.client.ui.PluginPanel;
+import net.runelite.client.util.ImageUtil;
 
 public class ItemSharePlayerPanel extends JPanel
 {
-	JTabbedPane tabs = new JTabbedPane();
+	JTabbedPane tabs = new JTabbedPane(JTabbedPane.TOP, JTabbedPane.SCROLL_TAB_LAYOUT);
 
 	ItemShareEquipmentPanel equipment;
 	ItemShareInventoryPanel inventory;
@@ -24,12 +36,18 @@ public class ItemSharePlayerPanel extends JPanel
 		inventory = new ItemShareInventoryPanel();
 		bank = new ItemShareItemListPanel();
 
-		tabs.addTab("Equipment", equipment);
-		tabs.addTab("Inventory", inventory);
-		tabs.addTab("Bank", bank);
+		tabs.addTab("Gear", getIcon(ICON_EQUIPMENT), equipment, "Equipment");
+		tabs.addTab("Inventory", getIcon(ICON_INVENTORY), inventory, "Inventory");
+		tabs.addTab("Bank", getIcon(ICON_BANK), bank, "Bank");
 		tabs.setAlignmentX(CENTER_ALIGNMENT);
 
 		add(tabs, BorderLayout.NORTH);
+	}
+
+	private ImageIcon getIcon(String path)
+	{
+		Image image = ImageUtil.loadImageResource(ItemSharePlugin.class, path);
+		return new ImageIcon(image.getScaledInstance(16, 16, SCALE_SMOOTH));
 	}
 
 	public void update(ItemManager itemManager, ItemSharePlayer player)
